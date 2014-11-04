ed0d39d1099bd3ac5fa5bde4fb5ffecae09aaf12
Martin <martin.tor.gar@gmail.com>
2014-11-04 15:00:48 -0800

    Database for the simpleCRUD system coded using PHP

---
+++ simplecrud.sql
@@ -0,0 +1,29 @@
+-- ----------------------------------------------------------------------------
+-- MySQL Workbench Migration
+-- Migrated Schemata: simpleCrud
+-- Source Schemata: simplecrud
+-- Created: Tue Nov 04 14:56:08 2014
+-- ----------------------------------------------------------------------------
+
+SET FOREIGN_KEY_CHECKS = 0;;
+
+-- ----------------------------------------------------------------------------
+-- Schema simpleCrud
+-- ----------------------------------------------------------------------------
+DROP SCHEMA IF EXISTS `simpleCrud` ;
+CREATE SCHEMA IF NOT EXISTS `simpleCrud` ;
+
+-- ----------------------------------------------------------------------------
+-- Table simpleCrud.users
+-- ----------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `simpleCrud`.`users` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(20) CHARACTER SET 'utf8' NOT NULL,
+  `lastname` VARCHAR(20) CHARACTER SET 'utf8' NOT NULL,
+  `email` VARCHAR(50) CHARACTER SET 'utf8' NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 12
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_unicode_ci;
+SET FOREIGN_KEY_CHECKS = 1;;
