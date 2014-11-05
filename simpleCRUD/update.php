1a2e8936d2d9c891507f226e11bcf607aaa599ab
Martin <martin.tor.gar@gmail.com>
2014-11-05 12:02:04 -0800

    Added email validation

--- simpleCRUD/update.php
+++ simpleCRUD/update.php
@@ -2,16 +2,21 @@
 
 require_once 'init.php';
 
-
 try{
-  $id = isset($_GET['id']) ? $_GET['id'] : $_POST['id'];    
+  
+  $id = isset($_GET['id']) ? $_GET['id'] : $_POST['id'];      
   $name = isset($_GET['name']) ? $_GET['name'] : $_POST['name'];
   $lastname = isset($_GET['lastname']) ? $_GET['lastname'] : $_POST['lastname'];
-  $email = isset($_GET['email']) ? $_GET['email'] : $_POST['email'];
-
+  $email = isset($_GET['email']) ? $_GET['email'] : $_POST['email'];  
   $stmt = $db->prepare('UPDATE users SET name=?, lastname=?, email=? WHERE id=?');  
-
-  if($_SERVER['REQUEST_METHOD'] == "POST")  
+ 
+  if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+    $error = ' Invalid email';    
+  } else {
+    $error = '';
+  }  
+  
+  if(($_SERVER['REQUEST_METHOD'] == "POST") && empty($error)) 
     if($stmt->execute(array($name, $lastname, $email, $id))){
       echo '<br/>', 'Updated' , '<br/>';
   } 
@@ -30,20 +35,19 @@ try{
 <?php try { ?>
  <form action="update.php" method="post">
   <div class="container">
-    <label for="id">ID</label>    
-    <input type="text" name="id" value="<?php echo $id; ?>" READONLY/><br/>   
+    <input type="text" name="id" value="<?php echo $id; ?>" READONLY hidden/><br/>   
     <label for="name">Name</label> 
     <input type="text" name="name" value="<?php echo $name; ?>"/><br/>
     <label for="lastname">Lastname</label>    
     <input type="text" name="lastname" value="<?php echo $lastname; ?>" /><br/>
     <label for="email">Email</label>
-    <input type="email" name="email" value="<?php echo $email; ?>"/><br/>    
+    <input type="email" name="email" value="<?php echo $email;?>"/><span><?php echo $error;?></span><br/>    
     <input type="submit" value="Ok"><br/><br/>
     <a href="index.php">Back</a>    
   </div>      
   </form>
   <?php }catch(Exception $e){ ?>
   <?php header('Location: index.php'); ?>
-  <?php } ?>
+  <?php } ?>  
 </body>
 </html>
