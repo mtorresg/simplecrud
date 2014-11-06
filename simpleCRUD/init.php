9c1d03d51a08648d10fda3766826bab5ec00c6cd
mtorresg <martin.tor.gar@gmail.com>
2014-11-05 23:58:07 -0800

    Database connection and validator object

--- simpleCRUD/init.php
+++ simpleCRUD/init.php
@@ -6,5 +6,4 @@ $db = new PDO('mysql:host=localhost;dbname=simpleCRUD;', 'root', '');
 $whoops = new \Whoops\Run;
 $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
 $whoops->register();
-
 ?>
